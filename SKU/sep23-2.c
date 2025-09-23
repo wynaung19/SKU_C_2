59e32da72d8c4e11b7be20dd8c73bc8b7fd9409f
Wai Yan Naung <waiyannaung19@gmail.com>
2025-09-23 09:56:18 +0900

    sep23 -2-1

--- SKU/sep23-2.c
+++ SKU/sep23-2.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+#define ROWS 3
+#define COLS 5
 int main(void) {
-	
+	int s[ROWS][COLS];
+
+	srand((unsigned)time(NULL));
+
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLS; j++) {
+			s[i][j] = rand() % 100;
+		}
+	}
+
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLS; j++) {
+			printf("%02d ", s[i][j]);
+		}
+		printf("\n");
+	}
+
 	return 0;
 }
