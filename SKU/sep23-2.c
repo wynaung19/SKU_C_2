ee84495a95df7b6d0381bd9969fac15813b9d23f
Wai Yan Naung <waiyannaung19@gmail.com>
2025-09-23 10:43:47 +0900

    #

--- SKU/sep23-2.c
+++ SKU/sep23-2.c
@@ -1,31 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-#define ROWS 3
-#define COLS 5
-int main(void) {
-	int s[ROWS][COLS];
-	float avg = 0.0;
-	int sum = 0;
-
-	srand((unsigned)time(NULL));
-
-	for (int i = 0; i < ROWS; i++) {
-		for (int j = 0; j < COLS; j++) {
-			scanf("%d", &s[i][j]);
-			sum += s[i][j];
-		}
-	}
-
-	for (int i = 0; i < ROWS; i++) {
-		for (int j = 0; j < COLS; j++) {
-			printf("%02d ", s[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\nSum: %5d\n", sum);
-	printf("Avg: %5.2f\n", (float)sum / (ROWS * COLS) );
-	
-	return 0;
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//
+//#define ROWS 3
+//#define COLS 5
+//int main(void) {
+//	int s[ROWS][COLS];
+//	float avg = 0.0;
+//	int sum = 0;
+//
+//	srand((unsigned)time(NULL));
+//
+//	for (int i = 0; i < ROWS; i++) {
+//		for (int j = 0; j < COLS; j++) {
+//			scanf("%d", &s[i][j]);
+//			sum += s[i][j];
+//		}
+//	}
+//
+//	for (int i = 0; i < ROWS; i++) {
+//		for (int j = 0; j < COLS; j++) {
+//			printf("%02d ", s[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	printf("\nSum: %5d\n", sum);
+//	printf("Avg: %5.2f\n", (float)sum / (ROWS * COLS) );
+//	
+//	return 0;
+//}
