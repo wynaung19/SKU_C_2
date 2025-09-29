07f879d90df2eb43bf50596c514f2192f32a8ce3
Wai Yan Naung <waiyannaung19@gmail.com>
2025-09-29 19:21:09 +0900

    #

--- SKU/sep23-3.c
+++ SKU/sep23-3.c
@@ -1,17 +1,17 @@
-﻿#include <stdio.h>
-#define ROWS 3
-#define COLS 5
-
-int main(void) {
-	int i;
-	int a[ROWS][COLS] = { { 87, 98, 80, 76, 3 },
-		{ 99, 89, 90, 90, 0 },
-		{ 65, 68, 50, 49, 0 }
-	};
-	
-	for (i = 0; i < ROWS; i++) {
-		double final_scores = a[i][0] * 0.3 + a[i][1] * 0.4 + a[i][2] * 0.2 + a[i][3] * 0.1 - a[i][4];
-		printf("Stu %d score = %10.2f\n", i + 1, final_scores);
-	}
-	return 0;
-}
+﻿//#include <stdio.h>
+//#define ROWS 3
+//#define COLS 5
+//
+//int main(void) {
+//	int i;
+//	int a[ROWS][COLS] = { { 87, 98, 80, 76, 3 },
+//		{ 99, 89, 90, 90, 0 },
+//		{ 65, 68, 50, 49, 0 }
+//	};
+//	
+//	for (i = 0; i < ROWS; i++) {
+//		double final_scores = a[i][0] * 0.3 + a[i][1] * 0.4 + a[i][2] * 0.2 + a[i][3] * 0.1 - a[i][4];
+//		printf("Stu %d score = %10.2f\n", i + 1, final_scores);
+//	}
+//	return 0;
+//}
