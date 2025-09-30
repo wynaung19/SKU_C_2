735b7cddcef7667f83d5ef22dae283c4924288a1
Wai Yan Naung <waiyannaung19@gmail.com>
2025-09-30 09:32:35 +0900

    #

---
+++ SKU/sep-30.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(void) {
+
+	/*int i = 10;
+	char c = 69;
+	float f = 12.3;
+	printf("i addr: %u\n", &i); 
+	printf("c addr: %u\n", &c); 
+	printf("f addr: %u\n", &f);*/
+
+	/*int A = 10;
+	double B = 12.3;
+	int *p;
+	float *p2 = NULL;
+	p = &A;
+	p2 = &B;
+	printf("A addr: %u\n", &A);
+	printf("B addr: %u\n", &B);
+	printf("%.2f %.2f\n", A+B, *p + p2);*/
+
+	/*int i = 10;
+	int *p = NULL;
+	p = &i;
+	printf("i = %d\n", i);
+	*p = 20;
+	printf("i = %d\n", i);*/
+
+	// will cause error
+	/*int i;
+	double* pd;
+	pd = &i;
+	*pd = 36.5;
+	printf("i = %d\n", i);*/
+
+
+
+	return 0;
+}
