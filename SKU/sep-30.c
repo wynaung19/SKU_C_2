fb8cc486f0eb3127f64bd2123f5fabb7cb52011d
Wai Yan Naung <waiyannaung19@gmail.com>
2025-09-30 10:21:26 +0900

    #3

--- SKU/sep-30.c
+++ SKU/sep-30.c
@@ -47,7 +47,7 @@ int main(void) {
 	printf("pc   : %d\n", pc);
 	printf("pc+2 : %d\n", pc + 2);*/
 
-	char* pc;
+	/*char* pc;
 	int* pi;
 	double* pd;
 	pc = (char*)10000;
@@ -57,7 +57,24 @@ int main(void) {
 	pc++;
 	pi++;
 	pd++;
-	printf("pc = %d,pi= %d,pd = %d\n", pc, pi, pd);
+	printf("pc = %d,pi= %d,pd = %d\n", pc, pi, pd);*/
+
+	/*int A = 10;
+	int *p = &A;
+	printf("A = %d\n", A);
+	(*p)++;
+	printf("A = %d\n", A);
+	A = 1;
+	++*p;
+	printf("A = %d\n", A);*/
+
+	int i = 10;
+	int* pi = &i;
+	printf("i = %d, pi = %p\n", *pi, pi);
+	(*pi)++;
+	printf("i = %d, pi = %p\n", *pi, pi);
+	(*pi)++;
+	printf("i = %d, pi = %p\n", i, pi);
 
 	return 0;
 }
