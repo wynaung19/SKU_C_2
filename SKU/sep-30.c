14d71a2e4014b04e28a73344087b122895b55fa0
Wai Yan Naung <waiyannaung19@gmail.com>
2025-09-30 11:23:01 +0900

    #

--- SKU/sep-30.c
+++ SKU/sep-30.c
@@ -7,14 +7,27 @@
 //	y = temp;
 //	printf("x = %d, y = %d\n", x, y);
 //}
+
 // call by reference
-void swap(int* x, int* y) {
-	int temp;
-	temp = *x;
-	*x = *y;
-	*y = temp;
-	printf("x = %d, y = %d\n", *x, *y);
-}
+//void swap(int* x, int* y) {
+//	int temp;
+//	temp = *x;
+//	*x = *y;
+//	*y = temp;
+//	printf("x = %d, y = %d\n", *x, *y);
+//}
+
+//int get_line_parameter(int x1, int y1, int x2, int y2, float* slope, float* yintercept)
+//{
+//	if (x1 == x2)
+//		return -1;
+//	else {
+//		*slope = (float)(y2 - y1) / (float)(x2 - x1);
+//		*yintercept = y1 - (*slope) * x1;
+//		return 0;
+//	}
+//}
+
 int main(void) {
 
 	/*int i = 10;
@@ -106,15 +119,29 @@ int main(void) {
 		printf("*(pc + %d) = %02X\n", i, *(pc + i));
 	}*/
 
-	int a = 10, b = 20;
-	printf("a = %d, b = %d\n", a, b);
+	/*int a = 10, b = 20;
+	printf("a = %d, b = %d\n", a, b);*/
 	//call by value
 	/*swap(a, b);
 	printf("a = %d, b = %d\n", a, b);*/
 
 	// call by reference
-	swap(&a, &b);
-	printf("a = %d, b = %d\n", a, b);
+	/*swap(&a, &b);
+	printf("a = %d, b = %d\n", a, b);*/
+
+	/*float s, y;
+	if (get_line_parameter(3,3,6,6, &s, &y) == 0)
+		printf("slope = %.5f, yintercept = %.5f\n", s, y);
+	else
+		printf("error\n");*/
+
+	int a[5] = { 10,20,30,40,50 };
+
+	printf("&a[0] = %p\n", &a[0]);
+	printf("&a[1] = %u\n", &a[1]);
+	printf("&a[2] = %u\n", &a[2]);
+
+	printf("a   = %u\n", a);
 
 	return 0;
 }
