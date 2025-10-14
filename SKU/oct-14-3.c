0540c718a8147a1bc5bf80b8be96bb1699183258
Wai Yan Naung <waiyannaung19@gmail.com>
2025-10-14 11:27:08 +0900

    10-14 end

--- SKU/oct-14-3.c
+++ SKU/oct-14-3.c
@@ -7,7 +7,14 @@ struct student
 	char name[20];
 	double grade;
 };
-
+int equal(struct student *s1, struct student *s2);
+struct student create() {
+	struct student s;
+	s.number = 3;
+	strcpy(s.name, "park");
+	s.grade = 4.3;
+	return s;
+}
 int main(void)
 {
 	/*struct student s[SIZE];
@@ -36,11 +43,45 @@ int main(void)
 	////printf("add of A: %0x\n", &A);
 	//printf("add of pA: %p\n", &pA);
 
-	struct student s = { 1001, "Wai Yan Naung", 4.5 };
+	/*struct student s = { 1001, "Wai Yan Naung", 4.5 };
 	struct student* pS = NULL;
 	pS = &s;
 	printf("Student: %d, %s, %.1f\n", s.number, s.name, s.grade);
 	printf("Student: %d, %s, %.1f\n", (*pS).number, (*pS).name, (*pS).grade);
+	printf("Student: %d, %s, %.1f\n", pS->number, pS->name, pS->grade);*/
 
+	// Array of Structure with Pointer/-------------------
+	/*struct student s1 = { 1001, "Wai Yan Naung", 4.5 };
+	struct student s2 = { 1001, "Aung Aung", 4.0 };
+
+	if (equal(&s1, &s2) == 1)
+		printf("Same Number\n");
+	else if (equal(&s1, &s2) == 2)
+		printf("Same Name\n");
+	else if (equal(&s1, &s2) == 3)
+		printf("Same Grade\n");
+	else
+		printf("Not Equal\n");
+
+	return 0;*/
+
+	struct student a = { 20190001,"hong", 3.8 };
+	struct student b;
+	b = create();
+	printf("%d %s %.1lf\n", b.number,
+	b.name, b.grade);
 	return 0;
+
+}
+
+int equal(struct student* s1, struct student* s2)
+{
+	if (s1->number == s2->number)
+		return 1;
+	else if (s1->name == s2->name)
+		return 2;
+	else if (s1->grade == s2->grade)
+		return 3;
+	else
+		return 0;
 }
