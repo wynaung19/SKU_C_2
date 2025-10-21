46850102d987061cd1d12681435516edc92f4918
Wai Yan Naung <60054637+wynaung19@users.noreply.github.com>
2025-10-21 11:18:50 +0900

    final

--- SKU/hw2.c
+++ SKU/hw2.c
@@ -1,74 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h> 
 #include <string.h>
 
 #define STU_NUMBER 1
 #define REG_NUMBER 2
+
 struct student {
-	int type;
-	union {
-		int stu_number;
-		char reg_number[15];
-	}id;
-	char name[20];
-	int grade;
+    int type;
+    union {
+        int stu_number;
+        char reg_number[15];
+    } id;
+    char name[20];
+    int grade;
 };
 
+
 char grade_letter(int grade) {
-	if (grade >= 85) return 'A';
-	else if (grade >= 70) return 'B';
-	else if (grade >= 55) return 'C';
-	else if (grade >= 40) return 'D';
-	else return 'F';
+    if (grade >= 85) return 'A';
+    else if (grade >= 70) return 'B';
+    else if (grade >= 55) return 'C';
+    else if (grade >= 40) return 'D';
+    else return 'F';
 }
 
-int avg_grade(struct student s[], int n) {
-	int sum = 0;
-	for (int i = 0; i < n; i++) {
-		sum += s[i].grade;
-	}
-	return sum / n;
-}
 
 void print(struct student s) {
-	switch (s.type) {
-	case STU_NUMBER:
-		printf("STUNUM : %d\n", s.id.stu_number);
-		printf("NAME : %s\n", s.name);
-		break;
-	case REG_NUMBER:
-		printf("REGNUM : %s\n", s.id.reg_number);
-		printf("NAME : %s\n", s.name);
-		break;
-	default:
-		break;
-	}
+    if (s.type == STU_NUMBER) {
+        printf("%d\t%d\t%s\t%d\t%c\n",
+            s.type, s.id.stu_number, s.name, s.grade, grade_letter(s.grade));
+    }
+    else if (s.type == REG_NUMBER) {
+        printf("%d\t%s\t%s\t%d\t%c\n",
+            s.type, s.id.reg_number, s.name, s.grade, grade_letter(s.grade));
+    }
 }
 
 int main() {
-	struct student s1, s2, s3;
-	/*s1.type = STU_NUMBER;
-	s1.id.stu_number = 1001;
-	strcpy(s1.name, "Wai Yan Naung");
-	s2.type = REG_NUMBER;
-	strcpy(s2.id.reg_number, "REG2024001");
-	strcpy(s2.name, "Aung Aung");
-	print(s1);
-	print(s2);*/
-	printf("Enter type, id and name grade for stu1: ");
-	scanf("%d %d %s %d", &s1.type, &s1.id.stu_number, s1.name, &s1.grade);
-	printf("Enter type, id and name grade for stu2: ");
-	scanf("%d %s %s %d", &s2.type, s2.id.reg_number, s2.name, &s2.grade);
-	printf("Enter type, id and name grade for stu3: ");
-	scanf("%d %s %s %d", &s3.type, s3.id.reg_number, s3.name, &s3.grade);
+    int n;
+    printf("Enter number of students: ");
+    scanf("%d", &n);
 
-	printf("TYPE\tID\tNAME\tSCORE\tGRADE\n");
-	printf("%d\t%d\t%s\t%d\t%c\n", s1.type, s1.id.stu_number, s1.name, s1.grade, grade_letter(s1.grade));
-	printf("%d\t%s\t%s\t%d\t%c\n", s2.type, s2.id.reg_number, s2.name, s2.grade, grade_letter(s2.grade));
-	printf("%d\t%s\t%s\t%d\t%c\n", s3.type, s3.id.reg_number, s3.name, s3.grade, grade_letter(s3.grade));
 
-	struct student students[] = { s1, s2, s3 };
-	printf("Average Grade: %d\n", avg_grade(students, 3));
+    struct student* s = (struct student*)malloc(n * sizeof(struct student));
+    if (s == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
 
-	return 0;
+
+    for (int i = 0; i < n; i++) {
+        printf("\nEnter type (1=student number, 2=register number) for student %d: ", i + 1);
+        scanf("%d", &s[i].type);
+
+        if (s[i].type == STU_NUMBER) {
+            printf("Enter student number, name, and grade: ");
+            scanf("%d %s %d", &s[i].id.stu_number, s[i].name, &s[i].grade);
+        }
+        else if (s[i].type == REG_NUMBER) {
+            printf("Enter register number, name, and grade: ");
+            scanf("%s %s %d", s[i].id.reg_number, s[i].name, &s[i].grade);
+        }
+        else {
+            printf("Invalid type. Please enter 1 or 2.\n");
+            i--;
+            continue;
+        }
+    }
+
+    printf("\nTYPE\tID\t\tNAME\tSCORE\tGRADE\n");
+    for (int i = 0; i < n; i++) {
+        print(s[i]);
+    }
+
+
+    free(s);
+    return 0;
 }
-
