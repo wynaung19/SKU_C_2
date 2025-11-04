f0bfbe7c404eed47f5c214d937ea91311d017146
Wai Yan Naung <60054637+wynaung19@users.noreply.github.com>
2025-11-04 10:37:53 +0900

    #

--- SKU/nov-4.c
+++ SKU/nov-4.c
@@ -1,31 +1,65 @@
-#include <stdio.h>
-
-void set_pointer(char** q) {
-	*q = "All that glisters is not gold.";
-}
-int main(void) {
-	//one
-	/*int i = 100;
-	int* p = &i;
-	int** q = &p;
-	*p = 200;
-	printf("i = %d\n", i);
-	**q = 300;
-	printf("i = %d\n", i);*/
-	//two
-	/*int i = 100;
-	int* p = &i;
-	int** q = &p;
-	**q = 200;
-	*p = 300;
-	printf("%d %d %d\n", i, *p, **q);*/
-	//three
-	/*char* p;
-	set_pointer(&p);
-	printf("오늘의 격언: %s \n", p);*/
-	//four
-	//char* p;
-	//set_pointer(p); //& 삭제 후 실행
-	//printf("오늘의 격언: %s \n", p);
-	return 0;
-}
+//#include <stdio.h>
+//
+//void set_pointer(char** q) {
+//	*q = "All that glisters is not gold.";
+//}
+////eight
+//int add(int, int);
+//int sub(int, int);
+//
+//int main(void) {
+//	//one
+//	/*int i = 100;
+//	int* p = &i;
+//	int** q = &p;
+//	*p = 200;
+//	printf("i = %d\n", i);
+//	**q = 300;
+//	printf("i = %d\n", i);*/
+//	//two
+//	/*int i = 100;
+//	int* p = &i;
+//	int** q = &p;
+//	**q = 200;
+//	*p = 300;
+//	printf("%d %d %d\n", i, *p, **q);*/
+//	//three
+//	/*char* p;
+//	set_pointer(&p);
+//	printf("오늘의 격언: %s \n", p);*/
+//	//four
+//	//char* p;
+//	//set_pointer(p); //& 삭제 후 실행
+//	//printf("오늘의 격언: %s \n", p);
+//	//five
+//	/*int i;
+//	int a = 10, b = 20, c = 30, d = 40, e = 50;
+//	int* pa[5] = { &a, &b, &c, &d, &e };
+//	for (i = 0; i < 5; i++)
+//		printf("%p %d\n", pa[i], *pa[i]);*/
+//
+//	//six
+//	/*char* fruits[] = { "apple", "banana", "cherry", "date", "elderberry" };
+//	int n = sizeof(fruits) / sizeof(fruits[0]);
+//	for (int i = 0; i < n; i++)
+//		printf("%s\n", fruits[i]);*/
+//	//seven
+//	/*int sub(int, int);
+//	int (*pf)(int, int);
+//	pf = sub;
+//	printf("Result: %d\n", pf(10, 5));*/
+//	int res;
+//	int (*pf[2])(int, int) = { add, sub };
+//	res = pf[0](10, 5);
+//	printf("Addition: %d\n", res);
+//	res = pf[1](10, 5);
+//	printf("Subtraction: %d\n", res);
+//
+//	return 0;
+//}
+//int add(int a, int b) {
+//	return a + b;
+//}
+//int sub(int a, int b) {
+//	return a - b;
+//}
--- SKU/test.c
+++ SKU/test.c
@@ -1,11 +1,26 @@
 //#include <stdio.h>
+//#include <string.h>
 //
-//int main(void) {
-//	int a = 5;
-//	int* pa = NULL;
-//	pa = &a;
-//	printf("Value of a: %d\n", a);
-//	printf("Address of a: %p\n", (void*)&a);
-//	printf("Value of pa (address of a): %p\n", (void*)pa);
-//	printf("Value pointed to by pa: %d\n", *pa);
+//struct Course {
+//	char code[10];
+//	char title[50];
+//	int creditHours;
+//};
+//
+//int main() {
+//	struct Course c[3];
+//	for (int i = 0; i < 3; i++) {
+//		printf("Enter course code: ");
+//		scanf("%s", c[i].code);
+//		printf("Enter course title: ");
+//		scanf(" %[^\n]s", c[i].title);
+//		printf("Enter credit hours: ");
+//		scanf("%d", &c[i].creditHours);
+//	}
+//	printf("\n%-10s %-50s %-15s\n", "Code", "Title", "Credit Hours");
+//	for (int i = 0; i < 3; i++) {
+//		printf("%-10s %-50s %-15d\n", c[i].code, c[i].title, c[i].creditHours);
+//	}
+//	
+//    return 0;
 //}
